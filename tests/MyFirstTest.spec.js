e53072e57dfc956d4c6a0d4a32b70a5c633c1c7a
VIVEKVPA <97500515+VIVEKVPA@users.noreply.github.com>
2026-01-25 01:28:42 +0530

    locator practice

--- tests/MyFirstTest.spec.js
+++ tests/MyFirstTest.spec.js
@@ -30,3 +30,83 @@ test('Customer Registration', async({page})=> {
     await page.locator('input[type="submit"][value="Continue"]').click()
 })
 
+test('Login to RSAcademy - Invalid Credentials', async ({browser}) => {
+
+    const context = await browser.newContext()
+    const page = await context.newPage()
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/')
+    const usernameInput = page.getByRole('textbox', { name: 'Username:', exact: true });
+    await usernameInput.fill('rahulshettyacademy');
+    const passwordInput = page.getByRole('textbox', { name: 'Password:', exact: true });
+    await passwordInput.fill('Learning@');
+    const termsCheckbox = page.locator(`#terms`);
+    await termsCheckbox.check();
+    const signInBtnButton = page.getByRole('button', { name: 'Sign In', exact: true });
+    await signInBtnButton.click();
+
+    const expected_error_message = 'Incorrect username/password.'
+    const incorrectUsernamePasswordText = page.getByText('Incorrect username/password.', { exact: true });
+    await expect(incorrectUsernamePasswordText).toBeVisible()
+    // const actual_error_message = await incorrectUsernamePasswordText.innerText()
+    const actual_error_message = await incorrectUsernamePasswordText.textContent()
+    expect(actual_error_message).toBe(expected_error_message)
+    await expect(incorrectUsernamePasswordText).toHaveText(expected_error_message)
+})
+
+test('Login to RSAcademy - valid Credentials', async ({page}) => {
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/')
+    const usernameInput = page.getByRole('textbox', { name: 'Username:', exact: true });
+    await usernameInput.fill('rahulshettyacademy');
+    const passwordInput = page.getByRole('textbox', { name: 'Password:', exact: true });
+    await passwordInput.fill('Learning@830$3mK2');
+    const termsCheckbox = page.locator(`#terms`);
+    await termsCheckbox.check();
+    const signInBtnButton = page.getByRole('button', { name: 'Sign In', exact: true });
+    await signInBtnButton.click();
+    const shopNameHeading = page.getByRole('heading', { name: 'Shop Name', exact: true, level: 1 });
+    const expected_text = 'Shop Name'
+    const actual_text = await shopNameHeading.textContent();
+    await expect(actual_text).toEqual(expected_text)
+    const card_titles = page.locator('.card-body a')
+    const iphoneXLink = await card_titles.nth(0).textContent();
+    const title_list = await card_titles.allTextContents();
+    console.log(iphoneXLink)
+    console.log(title_list)
+    expect(title_list).toContain('iphone X')
+})
+
+test('Register to RSAcademy', async ({page}) => {
+
+    await page.goto("https://rahulshettyacademy.com/client/#/auth/login")
+    // const registerHereLink = page.getByRole('link', { name: 'Register here', exact: true });
+    const registerHereLink = page.locator('a.text-reset');
+    await registerHereLink.click();
+    await page.locator(`#firstName`).fill('Vivek');
+    await page.locator(`#lastName`).fill('VPA');
+    await page.locator('#userEmail').fill('vivek123@example.com')
+    await page.locator('#userMobile').fill('2563458563')
+    await page.locator('select.custom-select').selectOption('Doctor')
+    const maleRadio = page.getByRole('radio', { name: 'Male', exact: true, checked: false });
+    await maleRadio.check();
+    // await page.getByRole('radio').check()
+    await page.locator('#userPassword').fill('Hknd@u72')
+    await page.locator('#confirmPassword').fill('Hknd@u72')
+    await page.getByRole('checkbox').check()
+    await page.locator('#login').click()
+
+})
+
+test.only('Login to RSAcademy', async ({page}) => {
+    await page.goto("https://rahulshettyacademy.com/client/#/auth/login")
+    await page.locator('#userEmail').fill('vivek123@example.com')
+    await page.locator('#userPassword').fill('Hknd@u72')
+    await page.locator('#login').click()
+    await expect(page).toHaveURL('https://rahulshettyacademy.com/client/#/dashboard/dash')
+    const loginSuccessfullyText = page.getByText('Login Successfully', { exact: true });
+    await expect(loginSuccessfullyText).toBeVisible()
+    await page.waitForLoadState('networkidle')
+    const card_titles = await page.locator('.card-body b').allTextContents()
+    console.log(card_titles)
+    expect(card_titles).toContain('ADIDAS ORIGINAL')
+})
+
